9eec0abcac09857232a7cb8883ef3f9204cc3495
Dushyant Bansal <dushyant37@gmail.com>
2012-07-18 18:34:05 +0530

    Intermediate stage: implementing some methods in controller and datastore

--- src/main/java/sample/controllers/TwiminiController.java
+++ src/main/java/sample/controllers/TwiminiController.java
@@ -2,7 +2,16 @@ package sample.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import sample.service.TwiminiStore;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,9 +24,40 @@ import sample.service.TwiminiStore;
 @Controller
 public class TwiminiController {
     TwiminiStore tStore;
+    int userID;
 
     @Autowired
     public TwiminiController(TwiminiStore tStore){
         this.tStore = tStore;
     }
+
+    @RequestMapping(value = "/newpost.json", method = RequestMethod.POST)
+    @ResponseBody
+    Hashtable<String, String> newPostJson(@RequestParam String post){
+    // post, userID
+        Hashtable hs = new Hashtable<String, String>();
+        tStore.addPost(userID, post);
+        hs.put("status","Added Successfully");
+        return hs;
+    }
+
+
+    @RequestMapping(value = "/newfollower.json", method = RequestMethod.POST)
+    @ResponseBody
+    Hashtable<String, String> newFollowerJson(@RequestParam int following){
+        Hashtable hs = new Hashtable<String, String>();
+        tStore.addFollower(following, userID);
+        hs.put("status","Added Successfully");
+        return hs;
+    }
+
+    @RequestMapping(value = "/posts.json", method = RequestMethod.GET)
+    @ResponseBody
+    List<Map<String, String>> getPostsJson(){
+        List<Map<String, Object>> posts =  tStore.getPosts(userID);
+
+    }
+
+
+
 }
--- src/main/java/sample/service/TwiminiStore.java
+++ src/main/java/sample/service/TwiminiStore.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dushyant
@@ -19,5 +23,21 @@ public class TwiminiStore {
     @Autowired
     public TwiminiStore(SimpleJdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addPost(int userID, String post) {
+        jdbcTemplate.update("INSERT INTO post (user_id, post) VALUES (?,?)", userID, post);
+    }
+
+    public List<Map<String, Object>> getPosts(int userID){
+        List<Map<String, Object>> posts = jdbcTemplate.queryForList("select * from posts where user_id=?",userID);
+        if (posts == null)
+            posts = new ArrayList<Map<String, Object>>();
+
+        return posts;
+    }
+
+    public void addFollower(int following, int follower) {
+        jdbcTemplate.update("INSERT INTO followers (user_id, follower) VALUES (?,?)", following, follower);
     }
 }
