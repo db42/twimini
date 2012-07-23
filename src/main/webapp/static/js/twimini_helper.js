0cfa4ef4a481ecb158ba28eeea37068dbf06f3bb
Dushyant Bansal <dushyant37@gmail.com>
2012-07-23 18:50:56 +0530

    move javascript code from jsp page into separate helper file.

---
+++ src/main/webapp/static/js/twimini_helper.js
@@ -0,0 +1,37 @@
+function starter_script() {
+    $.get('/posts.json',
+        function(data){
+            jQuery.each(data, function(){
+                var tweets = $(new EJS({
+                    url: '/static/ejs/addTweet.ejs'}).render(this));
+            $('#tweetlist').append(tweets);
+            })
+
+    })
+    $.get('/followers.json',
+        function(data){
+            jQuery.each(data, function(){
+                var follower = $(new EJS({
+                    url: '/static/ejs/addUser.ejs'}).render(this));
+            $('#followerslist').append(follower);
+            })
+
+    })
+    $.get('/followings.json',
+        function(data){
+            jQuery.each(data, function(){
+                var following = $(new EJS({
+                    url: '/static/ejs/addUser.ejs'}).render(this));
+            $('#followinglist').append(following);
+            })
+
+    })
+}
+
+function add_tweet(form){
+    $.post('/posts.json',$(form).serialize(), function(data){
+        var tweet = $(new EJS({
+            url: '/static/ejs/addTweet.ejs'}).render(data));
+        $('#tweetlist').append(tweet);
+    });
+}
