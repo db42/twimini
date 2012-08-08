f4e86e756c5c385d39a9ea9b656b0f2399b5e45f
Ayush <ayush.goel2012@coe.dce.edu>
2012-08-08 18:02:37 +0530

    Account tab of settings - validation done

--- src/main/webapp/static/js/settings_helper.js
+++ src/main/webapp/static/js/settings_helper.js
@@ -1,11 +1,43 @@
 var tm = tm || {};
 
+function check_username(username){
+    $('#username-error').empty();
+    if(username.length === 0){
+        $('#username-error').append("User Name Empty").slideDown("slow");
+        return false;
+    }
+    if(username.search(' ') !== -1 || username.search('@') !== -1) {
+        $('#username-error').append("User Name can't have spaces or '@'").slideDown("slow");
+        return false;
+    }
+    if(username.length > 125){
+        $('#username-error').append("User Name Too Large").slideDown("slow");
+        return false;
+    }
+    return true;
+}
+function check_email(email){
+    $('#email-error').empty();
+    var re_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+    if(!re_email.test(email)){
+        $('#email-error').append("Email incorrect!").slideDown("slow");
+        return false;
+    }
+    if(username.length > 125){
+        $('#email-error').append("Email Too Large").slideDown("slow");
+        return false;
+    }
+    return true;
+}
+
 function password_change(form) {
     tm.auth_ajax("/update_password?userID=".concat(tm.userID), form, function (data) {tm.callNormal("Password updated successfully."); });
 }
 
 function account_change(form) {
-    tm.auth_ajax("/update_account?userID=".concat(tm.userID), form, function (data) {tm.callNormal("Account updated successfully."); });
+    if (check_username(form["username"].value) && check_email(form["email"].value)){
+        tm.auth_ajax("/update_account?userID=".concat(tm.userID), form, function (data) {tm.callNormal("Account updated successfully."); });
+    }
 }
 
 function profile_change(form) {
