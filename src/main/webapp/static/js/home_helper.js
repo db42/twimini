eeb9b3e6d2d73da3fa65d07cb3f1e20431100ef6
Ayush <ayush.goel2012@coe.dce.edu>
2012-08-13 23:02:18 +0530

    add-tweet js added, not yet working

--- src/main/webapp/static/js/home_helper.js
+++ src/main/webapp/static/js/home_helper.js
@@ -5,7 +5,7 @@ $(function () {
     tm.fill_topbar();
     $('#addpost-btn').click(function(){
         tweet_text = $('#tweet-text').val().trim();
-        alert(tweet_text);
+        console.log(tweet_text);
         add_tweet(tweet_text);
     })
 });
