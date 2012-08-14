07eb8617b2ef5a0ff35b619466947bf229ddf427
Dushyant Bansal <dushyant37@gmail.com>
2012-08-14 18:14:02 +0530

    refactor profile helper js code out of helper.

--- src/main/webapp/static/js/helper.js
+++ src/main/webapp/static/js/helper.js
@@ -294,115 +294,7 @@ tm.getProfileUserid = function () {
     }
 };
 
-function follow_user(user_id, caller_user_id) {
-    var url = '/users/' + caller_user_id + '/followings/' + user_id;
-    tm.auth_ajax(url, null, function (data) {
-        callError("Successfully followed.");
-    }, 'PUT');
-}
 
-function unfollow_user(user_id, caller_user_id) {
-    var url = '/users/' + caller_user_id + '/followings/' + user_id;
-    tm.auth_ajax(url, null, function (data) {
-        callError("Successfully unfollowed.");
-    }, 'DELETE');
-}
-var mouse_pressed = false;
-
-function activate_follow_button(user_id) {
-    var caller_user_id = tm.userID;
-    $('#fbutton').mouseenter(
-        function () {
-            if ($('#fbutton').hasClass('follow')) {
-                $('#fbutton').empty().append('Follow');
-            } else if ($('#fbutton').html().trim() === 'Following') {
-                $('#fbutton').empty().append('Unfollow');
-                $('#fbutton').addClass('unfollow');
-            }
-            mouse_pressed = false;
-        }
-    );
-    $('#fbutton').mousedown(
-        function () {
-            if ($('#fbutton').html().trim() === 'Unfollow') {
-                unfollow_user(user_id, caller_user_id);
-                $('#fbutton').removeClass('unfollow').addClass('follow');
-                $('#fbutton').empty().append('Follow');
-            } else if ($('#fbutton').html().trim() === 'Following') {
-                unfollow_user(user_id, caller_user_id);
-                $('#fbutton').addClass('follow');
-                $('#fbutton').empty().append('Follow');
-            } else {
-                follow_user(user_id, caller_user_id);
-                $('#fbutton').removeClass('follow');
-                $('#fbutton').empty().append('Following');
-            }
-            mouse_pressed = true;
-        }
-    );
-    $('#fbutton').mouseleave(
-        function () {
-            if (!mouse_pressed) {
-                if ($('#fbutton').html().trim() === 'Unfollow') {
-                    $('#fbutton').removeClass('follow').removeClass('unfollow');
-                    $('#fbutton').empty().append('Following');
-                }
-            }
-        }
-    );
-}
-
-function follow_user_button(user_block){
-    var caller_user_id = tm.userID;
-    var user_id = user_block.getAttribute('userid');
-
-    if(user_block.innerHTML.trim() === "Follow"){
-        follow_user(user_id, caller_user_id);
-        user_block.innerHTML = 'Following';
-        user_block.className = 'fubutton following';
-/*        //add to followings
-        var entity = $(new EJS({url: '/static/ejs/addUser.ejs'}).render(data));
-        $('.' + this.listName).append(entity);
-        var followingsview = new BasicView('addUser.ejs', 'followinglist', 'followings', userID);
-        followingsview.addOne();*/
-    }
-    else if(user_block.innerHTML.trim() === "Unfollow"){
-        unfollow_user(user_id, caller_user_id);
-        if(user_block.parentElement.parentElement.parentElement.parentElement.parentElement.id === "followers"){
-            user_block.innerHTML = 'Follow';
-            user_block.className = 'fubutton following follow';
-        }
-        else{
-            var parentx = user_block.parentNode.parentNode.parentNode;
-            parentx.parentNode.removeChild(parentx);
-        }
-    }
-}
-function follow_user_button_hover(user_block){
-    if(user_block.innerHTML.trim() === "Following"){
-        user_block.innerHTML = 'Unfollow';
-        user_block.className = 'fubutton unfollow';
-    }
-}
-function follow_user_button_out(user_block){
-    if(user_block.innerHTML.trim() === "Unfollow"){
-        user_block.innerHTML = 'Following';
-        user_block.className = 'fubutton following';
-    }
-}
-
-tm.add_user_info = function (user_id) {
-    $.get('/users/' + user_id + "?callerUserID=" + tm.userID, function (data) {
-        var entity = $(new EJS({url: '/static/ejs/UserInfo.ejs'}).render(data));
-        $('.profile-block').append(entity);
-        if (user_id == tm. userID){
-            $('#fbutton').remove();
-        }
-        else{
-            activate_follow_button(user_id);
-        }
-    });
-};
 
 var logout = function() {
     sessionStorage.clear();
@@ -443,7 +335,9 @@ var repost = function (postID) {
 var canLoad = true;
 $(window).scroll(function () {
     if (1.06 * $(window).scrollTop() >= $(document).height() - $(window).height() && canLoad) {
+        setTimeout(function () {
         tm.scrollview.load_new_data();
+        }, 1000);
     }
 });
 
