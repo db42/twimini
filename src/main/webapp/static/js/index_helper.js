e7a42e958c0c0a2f7b72679fdd62dae944574559
Dushyant Bansal <dushyant37@gmail.com>
2012-08-15 20:49:35 +0530

    Implemented validation for index page -  login and register.

---
+++ src/main/webapp/static/js/index_helper.js
@@ -0,0 +1,33 @@
+var event_objects = {
+    'username': tm.check_username,
+    'email': tm.check_email,
+    'password': tm.check_password
+};
+
+
+function apply_validation_events(){
+    var login_element = $(".login-box input");
+    login_element.blur( function(){
+        var elem_name = event.target['name'];
+        var elem_value = event.target['value'];
+
+        if (elem_name in event_objects){
+            event_objects[elem_name](elem_value, 'login-');
+        }
+    });
+
+    var register_element = $(".register-box input");
+    register_element.blur( function(){
+        var elem_name = event.target['name'];
+        var elem_value = event.target['value'];
+
+        if (elem_name in event_objects){
+            event_objects[elem_name](elem_value, 'register-');
+        }
+    });
+
+}
+
+$(function(){
+    apply_validation_events();
+});
