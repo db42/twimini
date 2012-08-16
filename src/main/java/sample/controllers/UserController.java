1c3d15b5d8a40f3cca49f289963cec2fbe86408c
Ayush <ayush.goel2012@coe.dce.edu>
2012-08-16 22:57:39 +0530

    refactoring, renamed baseImageUrl to baseGravatarImageUrl - removed comment

--- src/main/java/sample/controllers/UserController.java
+++ src/main/java/sample/controllers/UserController.java
@@ -16,7 +16,7 @@ public class UserController {
     UserStore userStore;
     MD5Encoder md5Encoder;
 
-    static String baseImageUrl = "http://www.gravatar.com/avatar/";
+    static String baseGravatarImageUrl = "http://www.gravatar.com/avatar/";
 
     @Autowired
     public UserController(UserStore UserStore, MD5Encoder md5Encoder) {
@@ -26,39 +26,37 @@ public class UserController {
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     @ResponseBody
-    Hashtable<String, String> registerJson(@RequestParam String username, @RequestParam String email, @RequestParam String password){
-        String image_url = baseImageUrl.concat(md5Encoder.encodeString(email)); //generate gravatar image url
-        Hashtable hs = userStore.addUser(username, email, password, image_url);
-        return hs;
+    Hashtable<String, String> registerJson(@RequestParam String username,
+                                           @RequestParam String email,
+                                           @RequestParam String password){
+        String image_url = baseGravatarImageUrl.concat(md5Encoder.encodeString(email));
+        return userStore.addUser(username, email, password, image_url);
     }
 
     @RequestMapping(value = "/update_password", method = RequestMethod.POST)
     @ResponseBody
     Hashtable<String, String> updateUserPassword(@RequestParam String userID,
-                                         @RequestParam(required = false) String old_password,
-                                         @RequestParam(required = false) String new_password){
+                                                 @RequestParam(required = false) String old_password,
+                                                 @RequestParam(required = false) String new_password){
 
-        Hashtable hs = userStore.updateUserPassword(userID, old_password, new_password);
-        return hs;
+        return userStore.updateUserPassword(userID, old_password, new_password);
     }
 
     @RequestMapping(value = "/update_account", method = RequestMethod.POST)
     @ResponseBody
     Hashtable<String, String> updateUserAccount(@RequestParam String userID,
-                                                 @RequestParam(required = false) String username,
-                                                 @RequestParam(required = false) String email){
+                                                @RequestParam(required = false) String username,
+                                                @RequestParam(required = false) String email){
 
-        Hashtable hs = userStore.updateUserAccount(userID, username, email);
-        return hs;
+        return userStore.updateUserAccount(userID, username, email);
     }
 
     @RequestMapping(value = "/update_profile", method = RequestMethod.POST)
     @ResponseBody
     Hashtable<String, String> updateUserProfile(@RequestParam String userID,
-                                                 @RequestParam(required = false) String name,
-                                                 @RequestParam(required = false) String description){
+                                                @RequestParam(required = false) String name,
+                                                @RequestParam(required = false) String description){
 
-        Hashtable hs = userStore.updateUserProfile(userID, name, description);
-        return hs;
+        return userStore.updateUserProfile(userID, name, description);
     }
 }
