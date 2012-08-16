6b150c348fbc888df40e4d6c1ac6284835017e3b
Dushyant Bansal <dushyant37@gmail.com>
2012-08-16 14:17:38 +0530

    refactored settings helper. More to do.

--- src/main/webapp/static/js/settings_helper.js
+++ src/main/webapp/static/js/settings_helper.js
@@ -2,26 +2,25 @@ var tm = tm || {};
 
 var new_password;
 
-
-function check_name(name){
+function check_name(name) {
     $('#name-error').empty();
     var ck_name = /^.{3,250}$/;
-    if(!ck_name.test(name)){
+    if (!ck_name.test(name)) {
         $('#name-error').append("Name can have only Characters<br/> Min-Length : 3").slideDown("slow");
         return false;
     }
     return true;
 }
-function check_description(description){
-    if(description.length > 250){
+function check_description(description) {
+    if (description.length > 250) {
         $('#description-error').empty().append("Description too large!").slideDown("slow");
         return false;
     }
     return true;
 }
 
-function check_password(newpassword, retype){
-    if(newpassword !== retype){
+function check_password(newpassword, retype) {
+    if (newpassword !== retype) {
         $('#password-error').empty().append("The retyped Password doesn't match!").slideDown("slow");
         return false;
     }
@@ -29,29 +28,29 @@ function check_password(newpassword, retype){
     return true;
 }
 
-function save_password(){
+function save_password() {
     new_password = event.target.value;
 }
 
-function validate_password(){
+function validate_password() {
     var red_password = event.target.value;
     check_password(new_password, red_password);
 }
 
 function password_change(form) {
-    if(check_password(form["new_password"].value, form["red_password"].value)){
+    if (check_password(form.new_password.value, form.red_password.value)) {
         tm.auth_ajax("/update_password?userID=".concat(tm.userID), form, function (data) {tm.callGlobalMessage("Password updated successfully."); });
     }
 }
 
 function account_change(form) {
-    if (tm.check_username(form["username"].value) && tm.check_email(form["email"].value)){
+    if (tm.check_username(form.username.value) && tm.check_email(form.email.value)) {
         tm.auth_ajax("/update_account?userID=".concat(tm.userID), form, function (data) {tm.callGlobalMessage("Account updated successfully."); });
     }
 }
 
 function profile_change(form) {
-    if(check_name(form["name"].value) && check_description(form["description"].value)){
+    if (check_name(form.name.value) && check_description(form.description.value)) {
         tm.auth_ajax("/update_profile?userID=".concat(tm.userID), form, function (data) {tm.callGlobalMessage("Profile updated successfully."); });
     }
 }
@@ -66,13 +65,13 @@ var event_objects = {
 };
 
 
-function apply_validation_events(){
-    var element = $("input");
-    element.blur( function(){
-        var elem_name = event.target['name'];
-        var elem_value = event.target['value'];
+function apply_validation_events() {
+    var elem_name, elem_value, element = $("input");
+    element.blur(function () {
+        elem_name = event.target.name;
+        elem_value = event.target.value;
 
-        if (elem_name in event_objects){
+        if (elem_name in event_objects) {
             event_objects[elem_name](elem_value);
         }
     });
@@ -82,7 +81,7 @@ function apply_validation_events(){
 $(function () {
     tm.add_user_info(tm.userID);
     activatables('tab', ['account', 'profile', 'password']);
-    $('fbutton').remove();
+    $('#fbutton').remove();
     tm.fill_topbar();
 
     apply_validation_events();
