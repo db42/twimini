8d52953c3eb1a051453619742085c3ceb3d114dc
Dushyant Bansal <dushyant37@gmail.com>
2012-08-16 12:37:25 +0530

    Clean field_validator.js

--- src/main/webapp/static/js/field_validator.js
+++ src/main/webapp/static/js/field_validator.js
@@ -1,22 +1,21 @@
-tm = tm || {};
+var tm = tm || {};
 
-tm.check_username = function (username, selector_prefix){
+tm.check_username = function (username, selector_prefix) {
     if (typeof selector_prefix === 'undefined') {
         selector_prefix = '#';
-    }
-    else {
+    } else {
         selector_prefix = '#' + selector_prefix;
     }
     $(selector_prefix + 'username-error').empty();
-    if(username.length === 0){
+    if (username.length === 0) {
         $(selector_prefix + 'username-error').append("User Name Empty").slideDown("slow");
         return false;
     }
-    if(username.search(' ') !== -1 || username.search('@') !== -1) {
+    if (username.search(' ') !== -1 || username.search('@') !== -1) {
         $(selector_prefix + 'username-error').append("User Name can't have spaces or '@'").slideDown("slow");
         return false;
     }
-    if(username.length > 125){
+    if (username.length > 125) {
         $(selector_prefix + 'username-error').append("User Name Too Large").slideDown("slow");
         return false;
     }
@@ -24,20 +23,20 @@ tm.check_username = function (username, selector_prefix){
     return true;
 };
 
-tm.check_email = function (email, selector_prefix){
+tm.check_email = function (email, selector_prefix) {
+    var re_email;
     if (typeof selector_prefix === 'undefined') {
         selector_prefix = '#';
-    }
-    else {
+    } else {
         selector_prefix = '#' + selector_prefix;
     }
     $(selector_prefix + 'email-error').empty();
-    var re_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
-    if(!re_email.test(email)){
+    re_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+    if (!re_email.test(email)) {
         $(selector_prefix + 'email-error').append("Invalid e-mail address").slideDown("slow");
         return false;
     }
-    if(email.length > 125){
+    if (email.length > 125) {
         $(selector_prefix + 'email-error').append("E-mail Too Large").slideDown("slow");
         return false;
     }
@@ -48,12 +47,11 @@ tm.check_email = function (email, selector_prefix){
 tm.check_password = function (password, selector_prefix){
     if (typeof selector_prefix === 'undefined') {
         selector_prefix = '#';
-    }
-    else {
+    } else {
         selector_prefix = '#' + selector_prefix;
     }
     $(selector_prefix + 'password-error').empty();
-    if(password.length < 6){
+    if (password.length < 6) {
         $(selector_prefix + 'password-error').append("At least 6 characters long").slideDown("slow");
         return false;
     }
