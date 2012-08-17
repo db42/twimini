931c638bcfabd0d54beedcc9512aee115ead8a40
Dushyant Bansal <dushyant37@gmail.com>
2012-08-17 10:24:40 +0530

    cleaned search_helper.js

--- src/main/webapp/static/js/search_helper.js
+++ src/main/webapp/static/js/search_helper.js
@@ -1,14 +1,14 @@
 var tm = tm || {};
 
-searchresult = function(searchterm){
-
-    tm.searchview = new BasicView('addUser.ejs', 'resultlist', 'search' + "?q="+searchterm, tm.userID);
+var fetchSearchResult = function (search_term) {
+    tm.searchview = new BasicView('addUser.ejs', 'resultlist', 'search' + "?q=" + search_term, tm.userID);
     tm.searchview.populate();
 };
 
 var getFromUrl = function (request_param) {
-    var queryparams, query, queries = document.location.href.split('?');
-    for (var i=0; i<queries.length; i++) {
+    var i, queryparams, query, queries;
+    queries = document.location.href.split('?');
+    for (i = 0; i < queries.length; i++) {
         query = queries[i];
         console.log(query);
         queryparams = query.split('=');
@@ -21,16 +21,16 @@ var getFromUrl = function (request_param) {
 };
 
 $(function () {
+    var search_term;
     tm.fill_topbar();
-    var searchterm = getFromUrl("q");
-    console.log("searchterm " + searchterm);
+    search_term = getFromUrl("q");
 
     //todo:include spaces
-    if (searchterm.search(' ')!==-1)
+    if (search_term.search(' ') !== -1)
         tm.callGlobalMessage("No spaces allowed for search");
 
-    if (searchterm !== null)
-        searchresult(searchterm);
+    if (search_term !== null)
+        fetchSearchResult(search_term);
 
     $('#addpost-btn').click(function(){
         tweet_text = $('#tweet-text').val().trim();
@@ -43,9 +43,9 @@ $(function () {
     });
 
     $('#tweet-text').keyup(function(){
-        t_length = $('#tweet-text').val().trim().length;
-        $('#tweet-length').empty().append(140-t_length);
-        if (t_length > 140){
+        tweet_length = $('#tweet-text').val().trim().length;
+        $('#tweet-length').empty().append(140-tweet_length);
+        if (tweet_length > 140){
             $('#tweet-length').addClass("color-red");
             $('#tweet-length').removeClass("lcolor");
         }
